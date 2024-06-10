18eb2fa3f41fca012bf0d7bbb7665d12cce4ef92
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2024-06-10 17:00:19 +0530

    add wifi row

--- hijacker.py
+++ hijacker.py
@@ -54,6 +54,66 @@ class Functions:
 
         return [aps, clients]
 
+class WifiRow(Gtk.ListBoxRow):
+    def __init__(self, ssid, bssid, manufacturer, pwr, sec, ch, other):
+        super(WifiRow, self).__init__()
+        self.ssid = ssid
+        self.bssid = bssid
+        self.manufacturer = manufacturer
+        self.pwr = pwr
+        self.sec = sec
+        self.ch = ch
+        self.other = other
+
+        self.set_size_request(500, 50)
+
+        # Create a button to hold the row content
+        button = Gtk.Button()
+        button.connect("clicked", self.on_button_clicked)
+
+        # Main container inside the button
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        button.add(hbox)
+
+        # Icon
+        icon = Gtk.Image.new_from_icon_name("network-wireless", Gtk.IconSize.MENU)
+        hbox.pack_start(icon, False, False, 0)
+
+        # Details Box
+        details_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
+        hbox.pack_start(details_box, True, True, 0)
+
+        # First Line: SSID, BSSID, Manufacturer
+        ssid_label = Gtk.Label(label=f"<b>{ssid}</b>", use_markup=True, xalign=0)
+        bssid_label = Gtk.Label(label=bssid, xalign=0)
+        manufacturer_label = Gtk.Label(label=manufacturer, xalign=1)
+        manufacturer_label.set_margin_left(20)
+        first_line = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        first_line.pack_start(ssid_label, True, True, 0)
+        first_line.pack_start(bssid_label, True, True, 0)
+        first_line.pack_start(manufacturer_label, False, False, 0)
+        details_box.pack_start(first_line, False, False, 0)
+
+        # Second Line: Power, Security, Channel, Other
+        pwr_label = Gtk.Label(label=f"PWR: {pwr}", xalign=0)
+        sec_label = Gtk.Label(label=f"SEC: {sec}", xalign=0)
+        ch_label = Gtk.Label(label=f"CH: {ch}", xalign=0)
+        other_label = Gtk.Label(label=other, xalign=0)
+        second_line = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        second_line.pack_start(pwr_label, True, True, 0)
+        second_line.pack_start(sec_label, True, True, 0)
+        second_line.pack_start(ch_label, True, True, 0)
+        second_line.pack_start(other_label, True, True, 0)
+        details_box.pack_start(second_line, False, False, 0)
+
+        # Add the button to the ListBoxRow
+        self.add(button)
+
+    def on_button_clicked(self, widget):
+        # Handle the button click event
+        print(f"Clicked on SSID: {self.ssid}, BSSID: {self.bssid}, Manufacturer: {self.manufacturer}, PWR: {self.pwr}, SEC: {self.sec}, CH: {self.ch}, Other: {self.other}")
+
+
 class Airodump(Functions):
     def __init__(self, builder):
         builder.get_object('btn_quit').connect('clicked', Gtk.main_quit)
@@ -74,8 +134,25 @@ class Airodump(Functions):
         tmp = 'start' if 'stop' in current else 'stop'
         self.btn_toggle_img.set_property('icon-name', f'media-playback-{tmp}')
 
-        if tmp == 'start':
-            Functions.get_output
+        if tmp == 'stop':
+            listbox = Gtk.ListBox()
+            listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+            self.ap_list.pack_start(listbox, False, False, 0)
+
+            # Sample Data
+            networks = [
+                ("CYTA", "38:D8:2F:XX:XX:XX", "ZTE Corporation", "-78", "WPA2", "1", "B:29 | D:0"),
+                ("Chris", "18:44:E6:XX:XX:XX", "ZTE Corporation", "-63", "WPA2", "3", "B:122 | D:0"),
+                ("Wind WiFi", "CC:7B:35:XX:XX:XX", "Unknown Manufacturer", "-86", "WPA2", "11", "B:9 | D:0"),
+                # Add more data as needed
+            ]
+
+            # Add rows to ListBox
+            for network in networks:
+                row = WifiRow(*network)
+                listbox.add(row)
+
+            self.ap_list.show_all()
 
     def add_btn(self):
         button = Gtk.Button(label=f"Button")
