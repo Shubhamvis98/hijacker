d6dfc14710d1f7eb205fb43ad9611dfbd4d77049
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2024-06-10 19:53:52 +0530

    update

--- hijacker.py
+++ hijacker.py
@@ -2,7 +2,7 @@
 # Author Shubham Vishwakarma
 # git/twitter: ShubhamVis98
 
-import gi, threading, subprocess, psutil, signal, csv
+import gi, threading, subprocess, psutil, signal, csv, os, glob
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GdkPixbuf, GLib
 
@@ -16,15 +16,16 @@ class AppDetails:
     applogo = f'{appinstallpath}/logo.svg'
 
 class Functions:
-    def execute_cmd(self, cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, cwd=None, bufsize=0):
+    def execute_cmd(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, cwd=None, bufsize=0):
         proc = subprocess.Popen(cmd.split(), stdout=stdout, stderr=stderr, stdin=stdin, cwd=cwd, bufsize=bufsize)
-        return [run.poll(), proc]
+        return proc
 
-    def interrupt_proc(self, proc):
+    def interrupt_proc(proc):
         os.kill(proc.pid, signal.SIGINT)
-        GLib.timeout_add(1000, terminate_proc, proc)
+        while proc.poll() is None:
+            pass
 
-    def terminate_proc(self, proc):
+    def terminate_proc(proc):
         if proc.poll() is None:
             proc.terminate()
 
@@ -54,8 +55,14 @@ class Functions:
 
         return [aps, clients]
 
+    def remove_files(name='_tmp'):
+        for filename in glob.glob(f'{name}*'):
+            if os.path.isfile(filename):
+                os.remove(filename)
+                print(f"Deleted: {filename}")
+
 class WifiRow(Gtk.ListBoxRow):
-    def __init__(self, ssid, bssid, manufacturer, pwr, sec, ch, other):
+    def __init__(self, ssid, bssid, manufacturer, pwr, sec, ch):
         super(WifiRow, self).__init__()
         self.ssid = ssid
         self.bssid = bssid
@@ -63,7 +70,6 @@ class WifiRow(Gtk.ListBoxRow):
         self.pwr = pwr
         self.sec = sec
         self.ch = ch
-        self.other = other
 
         self.set_size_request(500, 50)
 
@@ -85,25 +91,24 @@ class WifiRow(Gtk.ListBoxRow):
 
         # First Line: SSID, BSSID, Manufacturer
         ssid_label = Gtk.Label(label=f"<b>{ssid}</b>", use_markup=True, xalign=0)
-        bssid_label = Gtk.Label(label=bssid, xalign=0)
         manufacturer_label = Gtk.Label(label=manufacturer, xalign=1)
         manufacturer_label.set_margin_left(20)
         first_line = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         first_line.pack_start(ssid_label, True, True, 0)
-        first_line.pack_start(bssid_label, True, True, 0)
         first_line.pack_start(manufacturer_label, False, False, 0)
         details_box.pack_start(first_line, False, False, 0)
 
-        # Second Line: Power, Security, Channel, Other
+        # Second Line: Power, Security, Channel
+        bssid_label = Gtk.Label(label=bssid, xalign=0)
         pwr_label = Gtk.Label(label=f"PWR: {pwr}", xalign=0)
         sec_label = Gtk.Label(label=f"SEC: {sec}", xalign=0)
         ch_label = Gtk.Label(label=f"CH: {ch}", xalign=0)
-        other_label = Gtk.Label(label=other, xalign=0)
+        # other_label = Gtk.Label(label=other, xalign=0)
         second_line = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        second_line.pack_start(bssid_label, True, True, 0)
         second_line.pack_start(pwr_label, True, True, 0)
         second_line.pack_start(sec_label, True, True, 0)
         second_line.pack_start(ch_label, True, True, 0)
-        second_line.pack_start(other_label, True, True, 0)
         details_box.pack_start(second_line, False, False, 0)
 
         # Add the button to the ListBoxRow
@@ -111,7 +116,7 @@ class WifiRow(Gtk.ListBoxRow):
 
     def on_button_clicked(self, widget):
         # Handle the button click event
-        print(f"Clicked on SSID: {self.ssid}, BSSID: {self.bssid}, Manufacturer: {self.manufacturer}, PWR: {self.pwr}, SEC: {self.sec}, CH: {self.ch}, Other: {self.other}")
+        print(f"Clicked on SSID: {self.ssid}, BSSID: {self.bssid}, Manufacturer: {self.manufacturer}, PWR: {self.pwr}, SEC: {self.sec}, CH: {self.ch}")
 
 
 class Airodump(Functions):
@@ -131,34 +136,43 @@ class Airodump(Functions):
 
     def scan_toggle(self, widget):
         current = self.btn_toggle_img.get_property('icon-name')
-        tmp = 'start' if 'stop' in current else 'stop'
-        self.btn_toggle_img.set_property('icon-name', f'media-playback-{tmp}')
+        if 'start' in current:
+            self.proc = Functions.execute_cmd('airodump-ng -w _tmp --write-interval 1 --output-format csv,pcap --background 1 wlan1')
+            self.btn_toggle_img.set_property('icon-name', 'media-playback-stop')
+        else:
+            Functions.interrupt_proc(self.proc)
+            Functions.remove_files()
+            self.btn_toggle_img.set_property('icon-name', 'media-playback-start')
 
-        if tmp == 'stop':
-            listbox = Gtk.ListBox()
-            listbox.set_selection_mode(Gtk.SelectionMode.NONE)
-            self.ap_list.pack_start(listbox, False, False, 0)
+        # if tmp == 'stop':
+        #     listbox = Gtk.ListBox()
+        #     listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        #     self.ap_list.pack_start(listbox, False, False, 0)
 
-            # Sample Data
-            networks = [
-                ("CYTA", "38:D8:2F:XX:XX:XX", "ZTE Corporation", "-78", "WPA2", "1", "B:29 | D:0"),
-                ("Chris", "18:44:E6:XX:XX:XX", "ZTE Corporation", "-63", "WPA2", "3", "B:122 | D:0"),
-                ("Wind WiFi", "CC:7B:35:XX:XX:XX", "Unknown Manufacturer", "-86", "WPA2", "11", "B:9 | D:0"),
-                # Add more data as needed
-            ]
+        #     # Sample Data
+        #     networks = [
+        #         ("CYTA", "38:D8:2F:XX:XX:XX", "ZTE Corporation", "-78", "WPA2", "1"),
+        #         ("Chris", "18:44:E6:XX:XX:XX", "ZTE Corporation", "-63", "WPA2", "3"),
+        #         ("Wind WiFi", "CC:7B:35:XX:XX:XX", "Unknown Manufacturer", "-86", "WPA2", "11"),
+        #         # Add more data as needed
+        #     ]
 
-            # Add rows to ListBox
-            for network in networks:
-                row = WifiRow(*network)
-                listbox.add(row)
+        #     # Add rows to ListBox
+        #     for network in networks:
+        #         row = WifiRow(*network)
+        #         listbox.add(row)
 
-            self.ap_list.show_all()
+        #     self.ap_list.show_all()
 
     def add_btn(self):
         button = Gtk.Button(label=f"Button")
         button.set_size_request(-1, 50)
         self.ap_list.pack_start(button, True, True, 0)
         self.ap_list.show_all()
+
+    def get_aps(self):
+        aps, clients = Functions.extract_data('_tmp-01.csv')
+        print(aps, clients)
 
 class HijackerGUI(Gtk.Application):
     def __init__(self):
